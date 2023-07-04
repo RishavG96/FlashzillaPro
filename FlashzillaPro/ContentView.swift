3511ed6e4f3b856d5d93fec2d33cc03479858cda
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-07-04 08:21:39 +0530

    Initial Commit

---
+++ FlashzillaPro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlashzillaPro
+//
+//  Created by Rishav Gupta on 04/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlashzillaPro/FlashzillaProApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaProApp.swift
+//  FlashzillaPro
+//
+//  Created by Rishav Gupta on 04/07/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
