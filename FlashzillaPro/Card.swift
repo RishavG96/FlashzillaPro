9e882c303550d80190d2ca7f302f19ebfe5b669c
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-07-04 18:39:50 +0530

    Day 88 complete

---
+++ FlashzillaPro/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  FlashzillaPro
+//
+//  Created by Rishav Gupta on 04/07/23.
+//
+
+import Foundation
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jordie Whittaker")
+}
